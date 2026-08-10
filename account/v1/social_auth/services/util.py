946c8ce634d053335daa3d70af5bfb86c46797b2
Mark Okonedah <markedah9652@gmail.com>
2026-08-10 12:48:39 +0100

    Fix: Google Auth

--- account/v1/social_auth/services/util.py
+++ account/v1/social_auth/services/util.py
@@ -54,6 +54,6 @@ class SocialAuthService(CustomApiRequestProcessorBase, ABC):
                 "email": user.email,
                 "first_name": user.first_name,
                 "last_name": user.last_name,
-                "account_type": user.account_type,
+                "user_type": user.user_type,
             }
         }
